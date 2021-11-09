db21b85916f78482ada557e40323b2e2e657b19e
Ólafur Sverrir Kjartansson <kjarni@gmail.com>
2021-11-09 21:26:19 +0000

    v1

---
+++ test/main.test.js
@@ -0,0 +1,16 @@
+import { describe, expect, it } from '@jest/globals';
+import { reverse } from '../src/lib/reverse.js';
+
+describe('main.js', () => {
+  it('should reverse a single char string', () => {
+    const result = reverse('a');
+
+    expect(result).toBe('a');
+  });
+
+  it('should reverse a string of length > 1', () => {
+    const result = reverse('abc');
+
+    expect(result).toBe('cba');
+  });
+});
